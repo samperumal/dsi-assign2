e0660615776e41fcf42e207af419e1a3a618960b
Sam Perumal <sam_lki@yahoo.com>
2018-09-07 15:25:01 +0200

    Implemented parsing of input files to extract president/word occurrences

---
+++ sam/data-preprocessing.R
@@ -0,0 +1,39 @@
+require(tidyverse)
+
+filePath = "sona-text-1994-2018/"
+
+extractData = function(filePath) {
+  files = list.files(filePath, full.names = TRUE)
+
+  parseFilename = function(filename) {
+    matches = str_match(filename, "[^/]*/?(\\d{4})_(post|pre|)(?:_elections_|)(.+)\\.txt")
+    return (matches)
+  }
+
+  parseFile = function(filename) {
+    con = file(filename, open = "r")
+    lines = readLines(con, warn = FALSE)
+
+    close(con)
+
+    lines = lines[str_length(lines) > 0]
+
+    matches = parseFilename(filename)
+
+    result = data.frame(speech = unlist(lines), year = matches[2], election = matches[3], president = matches[4], stringsAsFactors = FALSE)
+
+    return(result)
+  }
+
+  sentences = lapply(files, parseFile) %>% bind_rows()  %>% unnest_tokens(sentence, speech, token = "sentences") %>% mutate(id = digest::digest(sentence))
+
+  return (list(
+    sentences = sentences,
+    words = sentences %>% unnest_tokens(word, sentence, token = "words")
+  ))
+}
+
+inputData = extractData(filePath)
+
+View(inputData$sentences)
+View(inputData$words)
