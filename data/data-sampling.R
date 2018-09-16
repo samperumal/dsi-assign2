befd04d1c60748886202d641821b6c12576e6ff9
Sam Perumal <sam_lki@yahoo.com>
2018-09-16 15:45:03 +0200

    Added pre-split test/validated .RData file

--- data/data-sampling.R
+++ data/data-sampling.R
@@ -9,11 +9,21 @@ load_test_and_train_data = function(df) {
   return (list(train = train, validate = validate))
 }
 
-#data = load_test_and_train_data(inputData$sentences)
+if (!exists("sentence_data")) {
+  filename = "sentence_data.RData"
+  if (file.exists(filename)) {
+    load(file = filename)
+  }
+  else {
+    source("data/data-preprocessing.R")
+    sentence_data = load_test_and_train_data(inputData$sentences)
+    save(sentence_data, file = filename)
+  }
+}
 
 # Attempt to ensure records per key are equal in length by supersampling
 balance_data = function(df, key_column = 1) {
   df #%>% group_by(.[[1]])
 }
 
-balance_data(data$train)
+#balance_data(data$train)
