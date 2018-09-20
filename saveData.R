01103bede5489ac0f3e034dbae017043d5e29eb7
Sam Perumal <sam_lki@yahoo.com>
2018-09-20 21:44:34 +0200

    Most of Merve's NN training images and results cached

---
+++ saveData.R
@@ -0,0 +1,7 @@
+save(test_accuracy_wordcount_bigger, train_accuracy_wordcount_bigger,
+     test_accuracy_wordcount_smaller, train_accuracy_wordcount_smaller, table_bow,
+     test_accuracy_tfidf, train_accuracy_tfidf, table_tfidf,
+     test_accuracy_sentiment, train_accuracy_sentiment, table_sentiment,
+     test_accuracy_topic, train_accuracy_topic, table_topic,
+
+     file = "mnn.RData")
